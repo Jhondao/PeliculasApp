468b684f493068a05ba5accf9b2915fa8ab9b3db
Jhon D Orozco <jhon.orozco@javerianacali.edu.co>
2023-02-03 16:23:05 -0500

    [ADD] hook de  useMovies

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native';
-import { useEffect } from 'react';
-import movieDB from '../api/movieDB';
+import { ActivityIndicator, View, Text } from 'react-native';
+import { useMovies } from '../hooks/useMovies';
 
 export const HomeScreen = () => {
 
-  useEffect(() => {
-    movieDB.get('/now_playing')
-        .then( resp => {
-          console.log(resp.data);
-        })
-  }, [])
-  
+  const {peliculasEnCine, isLoading} = useMovies();
 
+  if(isLoading){
+    return(
+      <View style={{flex:1, justifyContent: 'center',alignContent:'center'}}>
+        <ActivityIndicator color="red" size={50} />
+      </View>
+    )
+  }
+  
 
   return (
     <View>
